9dbed201d17c1d11dac32145c18c438cc6ea3ca0
Varga Csaba <csumasz84@gmail.com>
2023-11-05 10:13:40 +0100

    Home page

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome To PHPLoversBlog</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Playfair&#43;Display:700,900&amp;display=swap" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+   
+  </head>
+  <body>
+
+<div class="blog-masthead">
+  <div class="container">
+    <nav class="blog-nav">
+      <a class="blog-nav-item active" href="#">Home</a>
+      <a class="blog-nav-item" href="#">All posts</a>
+    </nav>
+  </div>
+</div>
+
+<main class="container">
+  <div class="blog-header">
+    <div class="logo">
+        <img src="images/php.jfif">
+    </div>
+    <div class="blog-header-text">
+        <h1 class="blog-title">PHP Lovers Blog</h1>
+        <p class="blog-description">PHP News, Tutorials, Videos  & More</p>
+    </div>
+  </div>
+    <div class="clear"></div>
+
+
+  <div class="row ">
+
+    <div class="col-sm-0 blog-main">
+        <div class="blog-post">
+            <h2 class="blog-post-title">Internal PHP Conference 2014</h2>
+            <p class="blog-post-meta"> January 1, 2014 by <a href="#">Mark</a></p>  
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula ante nec interdum hendrerit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur ac elit ut eros egestas tempus. Praesent dignissim enim vel urna tincidunt consectetur nec sodales urna. Sed ac risus ut tellus convallis rhoncus. Vivamus condimentum fringilla ipsum, sed vulputate quam dignissim nec. Etiam iaculis risus mauris, nec euismod ex tempor vitae. Nulla id sollicitudin mauris. Quisque porttitor tincidunt dolor sed maximus. Integer nec risus id sapien volutpat molestie vitae id nisi. In placerat dui id mauris condimentum ornare. Mauris accumsan nisl commodo egestas ullamcorper. Nullam vitae felis tincidunt, mollis massa ut, ullamcorper nisi.</p>
+            <a href="post.php?id=1" class="readmore">Read More</a>
+        </div>
+
+        <div class="blog-post">
+            <h2 class="blog-post-title">PHP 5.6.0beta4 Released</h2>
+            <p class="blog-post-meta"> December 13, 2023 by <a href="#">Jacob </a></p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula ante nec interdum hendrerit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur ac elit ut eros egestas tempus. Praesent dignissim enim vel urna tincidunt consectetur nec sodales urna. Sed ac risus ut tellus convallis rhoncus. Vivamus condimentum fringilla ipsum, sed vulputate quam dignissim nec. Etiam iaculis risus mauris, nec euismod ex tempor vitae. Nulla id sollicitudin mauris. Quisque porttitor tincidunt dolor sed maximus. Integer nec risus id sapien volutpat molestie vitae id nisi. In placerat dui id mauris condimentum ornare. Mauris accumsan nisl commodo egestas ullamcorper. Nullam vitae felis tincidunt, mollis massa ut, ullamcorper nisi.</p>
+            <a href="post.php?id=1" class="readmore">Read More</a>
+        </div>
+    </div>
+    <div class="col-sm-3 col-sm-offset blog-sidebar">
+        <div class="sidebar-module sidebar-module-inset">
+            <h4>About</h4>
+            <p></p>
+        </div>
+    </div>
+  </div>
+
+  
+
+</main>
+
+<footer class="py-5 text-center text-body-secondary bg-body-tertiary">
+  <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+  <p class="mb-0">
+    <a href="#">Back to top</a>
+  </p>
+</footer>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="js/bootstrap.js"></script>
+
+    </body>
+</html>
