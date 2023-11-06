d32c725acdb15004e8362d095b6e89cd27fd0fe5
Varga Csaba <csumasz84@gmail.com>
2023-11-06 16:56:21 +0100

    Sub pages

---
+++ includes/footer.php
@@ -0,0 +1,30 @@
+
+</div>
+    <div class="col-sm-3 col-sm-offset blog-sidebar">
+        <div class="sidebar-module sidebar-module-inset">
+            <h4>About</h4>
+            <p>Curabitur a quam <em>pellentesque</em> lacus vehicula fermentum. Nulla convallis lobortis placerat.</p>
+        </div>
+        <div class="sidebar-module">
+            <h4>Categories</h4>
+            <ol class="list-unstyled">
+                <li><a href="#">News</a></li>
+                <li><a href="#">Events</a></li>
+                <li><a href="#">Tutorials</a></li>
+                <li><a href="#">Misc</a></li>
+            </ol>
+        </div>
+    </div>
+    </div>
+</main>
+<footer class="blog-footer">
+        <p>PHPLoversBlog &copy; 2014</p>
+        <p >
+          <a href="#">Back to top</a>
+        </p>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+      <script src="js/bootstrap.js"></script>
+
+    </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome To PHPLoversBlog</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Playfair&#43;Display:700,900&amp;display=swap" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+   
+   
+  </head>
+  <body>
+
+      <div class="blog-masthead">
+        <div class="container">
+          <nav class="blog-nav">
+            <a class="blog-nav-item active" href="index.php">Home</a>
+            <a class="blog-nav-item" href="posts.php">All posts</a>
+          </nav>
+        </div>
+      </div>
+
+      <main class="container">
+        <div class="blog-header">
+          <div class="logo">
+              <img src="images/php.jfif">
+          </div>
+          <div class="blog-header-text">
+              <h1 class="blog-title">PHP Lovers Blog</h1>
+              <p class="blog-description">PHP News, Tutorials, Videos  & More</p>
+          </div>
+        </div>
+          <div class="clear"></div>
+
+
+        <div class="row ">
+
+          <div class="col-sm-9 blog-main">
